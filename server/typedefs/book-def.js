2ec28fb1eb7c4a02989161a5d78d0bd65bcf385e
Nelson <nelsonliang02@gmail.com>
2022-03-26 14:22:24 -0400

    Reverted changes -NL

--- server/typedefs/book-def.js
+++ server/typedefs/book-def.js
@@ -12,7 +12,6 @@ const typeDefs = gql`
   # The "Query" type is special: it lists all of the available queries that
   # clients can execute, along with the return type for each. In this
   # case, the "books" query returns an array of zero or more Books (defined above).
-  # test
   type Query {
     getBook: Book
   }
