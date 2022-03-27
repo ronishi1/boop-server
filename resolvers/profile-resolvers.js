6a671461021c30571e2fd49e0b177b4b4ad1aefb
Ray Onishi <ronishi@Rays-MacBook-Pro.local>
2022-03-27 16:54:31 -0400

    Finished profile resolvers, unfollow and follow mutations working

--- resolvers/profile-resolvers.js
+++ resolvers/profile-resolvers.js
@@ -13,20 +13,37 @@ module.exports = {
     }
   },
   Mutation: {
-    followUser: async (_, args, { res }) => {
+    followUser: async (_, args, { req,res }) => {
       // followID is the ID of the user to follow
       const { followID } = args;
-      const objId = new ObjectId(followID);
-      const followUser = await User.findOne({_id:objectId});
-      // followUser.followers.push(CURRENT USER ID GOES HERE);
+      const followUserID = new ObjectId(followID);
+      const currentUserID = new ObjectId(req.userId);
+      const followUser = await User.findOne({_id:followUserID});
+      const currentUser = await User.findOne({_id:currentUserID});
+      
+      // Add the current user as a follower to the other user
+      followUser.followers.push(currentUser._id);
+      // Add the followed user as a following for the current user
+      currentUser.following.push(followUser._id);
 
+      await User.updateOne({_id: currentUser._id}, {following: currentUser.following});
+      await User.updateOne({_id: followUser._id}, {followers: followUser.followers});
       return true;
     },
-    unfollowUser: async (_, args, { res }) => {
-      // followID is the ID of the user to follow
+    unfollowUser: async (_, args, { req,res }) => {
+      // followID is the ID of the user to unfollow
       const { followID } = args;
-      const objId = new ObjectId(followID);
-      const followUser = await User.findOne({_id:objectId});
+      const followUserID = new ObjectId(followID);
+      const currentUserID = new ObjectId(req.userId);
+      const followUser = await User.findOne({_id:followUserID});
+      const currentUser = await User.findOne({_id:currentUserID});
+      // Remove current user from followers of followed user
+      followUser.followers = followUser.followers.filter(f => f.toString() !== currentUser._id.toString());
+      // Remove followed user from followings of current user
+      currentUser.following = currentUser.following.filter(f => f.toString() !== followUser._id.toString());
+
+      await User.updateOne({_id: currentUser._id}, {following: currentUser.following});
+      await User.updateOne({_id: followUser._id}, {followers: followUser.followers});
 
       return true;
     },
